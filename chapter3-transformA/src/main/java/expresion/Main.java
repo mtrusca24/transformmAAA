4d1ffbb2c846f83781929f383d08bb2da75aa5df
mtrusca24 <34935055+mtrusca24@users.noreply.github.com>
2018-02-11 23:57:53 +0200

    Update Main.java

--- chapter3-transformA/src/main/java/expresion/Main.java
+++ chapter3-transformA/src/main/java/expresion/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		System.out.println("firstchange");
 		System.out.println("alooaa");
 
-		System.out.println("inca unaaaaaaaabbbcgg");
+		System.out.println("inca unaaaaaaaabbbcggkkkkkkkk");
 
 		CamelContext context=new DefaultCamelContext();
 		
