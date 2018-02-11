f1d34813f8fc396b1ef9f1c7059ea38cd61892de
Marius <Marius@Marius>
2018-02-11 22:21:47 +0200

    first change

--- chapter3-transformA/src/main/java/expresion/Main.java
+++ chapter3-transformA/src/main/java/expresion/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 	
-		
+		System.out.println("firstchange");
 		CamelContext context=new DefaultCamelContext();
 		
 		context.addRoutes(new Ex1RouteBuilder());
