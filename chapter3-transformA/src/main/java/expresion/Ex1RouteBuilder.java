e5af6ee6bb0d18d07542a7d62d4aff730f82ba0b
mtrusca24 <34935055+mtrusca24@users.noreply.github.com>
2018-02-14 21:58:17 +0200

    Update Ex1RouteBuilder.java

--- chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
+++ chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
@@ -20,7 +20,7 @@ public class Ex1RouteBuilder extends RouteBuilder {
 						
 						String reverse=arg0.getIn().getBody(String.class);
 						
-						
+						System.out.println();
 						
 						return (T) new StringBuilder(reverse).reverse().toString();
 					}
