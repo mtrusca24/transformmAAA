4624f2fb68b54b310fcdcaab16b07595dfff0355
Marius <Marius@Marius>
2018-03-15 19:48:41 +0200

    fgdgddggdgfdhgjfyhjk

--- chapter3-transformA/src/main/java/expresion/Main.java
+++ chapter3-transformA/src/main/java/expresion/Main.java
@@ -27,6 +27,8 @@ public class Main {
 		System.out.println("green");
 			System.out.println("a doua modficare");
 		System.out.println("a 40 modificare pe git");
+		System.out.println("seara");
+		System.out.println("a 40 modificare pe git");
 		context.start();
 		
 		ProducerTemplate template=context.createProducerTemplate();
