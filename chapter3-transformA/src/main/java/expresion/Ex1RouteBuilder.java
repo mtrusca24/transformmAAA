d07e2e3c4e3c8f800657a46cf623862610f8324d
mtrusca24 <34935055+mtrusca24@users.noreply.github.com>
2018-05-06 12:15:02 +0300

    Update Ex1RouteBuilder.java

--- chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
+++ chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
@@ -30,6 +30,7 @@ public class Ex1RouteBuilder extends RouteBuilder {
 						// capatane
 						//hei haiducuu
 						//as roma
+						//real madrid
 						String reverse=arg0.getIn().getBody(String.class);
 						System.out.println(" Marius's way");
 						System.out.println(" Jesus's way");
