3ea946c8dbd3d84c3024915b5fd548d61afe1e52
mtrusca24 <34935055+mtrusca24@users.noreply.github.com>
2018-05-06 16:01:38 +0300

    Update Ex1RouteBuilder.java

--- chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
+++ chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
@@ -47,6 +47,8 @@ public class Ex1RouteBuilder extends RouteBuilder {
 						// trebui mah
 						
 						// se poate remote
+						
+						// se poate iar remote
 						String reverse=arg0.getIn().getBody(String.class);
 						System.out.println(" Marius's way");
 						System.out.println(" Jesus's way");
