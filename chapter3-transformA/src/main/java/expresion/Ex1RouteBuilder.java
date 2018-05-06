9aac78e25472a228631123c214f86cc16c0bafe6
Marius <Marius@Marius>
2018-05-06 16:58:29 +0300

    Revert "Revert "alooo""
    This reverts commit 7f0706d0d2d207db925186dd75eaafdcbf1a1de4.

--- chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
+++ chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
@@ -61,6 +61,29 @@ public class Ex1RouteBuilder extends RouteBuilder {
 						//noi modificari
 						
 						//revert commit
+						
+						
+						//hai mahh  aloo
+						
+						
+						//mingeee
+						
+						//aloo
+						
+						
+						//mingeee
+						
+						// bahhhh
+						
+						
+						//mingeee
+						
+						// bahhhh
+						
+						
+						//mingeee
+						
+						// bahhhh
 						String reverse=arg0.getIn().getBody(String.class);
 						System.out.println(" Marius's way");
 						System.out.println(" Jesus's way");
