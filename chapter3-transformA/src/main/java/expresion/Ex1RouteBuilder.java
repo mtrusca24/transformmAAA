5f9fbd7f2b9958e84076c639081b173baa6f035c
Marius <Marius@Marius>
2018-05-06 13:27:06 +0300

    arsenal

--- chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
+++ chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
@@ -35,6 +35,8 @@ public class Ex1RouteBuilder extends RouteBuilder {
 						//steaua
 						
 						//craiova
+						
+						// arsenal
 						String reverse=arg0.getIn().getBody(String.class);
 						System.out.println(" Marius's way");
 						System.out.println(" Jesus's way");
