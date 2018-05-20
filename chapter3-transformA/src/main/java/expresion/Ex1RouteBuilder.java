476f63bf11cb77dcf0a24c6cee77e0001adfd638
mtrusca24 <truscamarius@gmail.com>
2018-05-20 19:17:06 +0300

    conflicte

--- chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
+++ chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
@@ -14,6 +14,8 @@ public class Ex1RouteBuilder extends RouteBuilder {
 		.transform(
 				
 				// cola dola
+				
+				// bobo
 				new Expression() {
 					
 					public <T> T evaluate(Exchange arg0, Class<T> arg1) {
