7f047b2672fb594d3f6a6dcb3d5d2bcc31918b5f
mtrusca24 <truscamarius@gmail.com>
2018-05-20 22:52:12 +0300

    llll

--- chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
+++ chapter3-transformA/src/main/java/expresion/Ex1RouteBuilder.java
@@ -33,6 +33,8 @@ public class Ex1RouteBuilder extends RouteBuilder {
 			// albert
 				
 				// alex
+				
+				//beni
 				new Expression() {
 					
 					public <T> T evaluate(Exchange arg0, Class<T> arg1) {
