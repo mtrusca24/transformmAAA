4fae8154bef5d1a2e9408aad7ba08f56733ff32f
mtrusca24 <34935055+mtrusca24@users.noreply.github.com>
2018-07-31 23:14:31 +0300

    Create euuu.java

---
+++ chapter3-transformA/src/main/java/expresion/euuu.java
@@ -0,0 +1,40 @@
+package camelinaction;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+	
+		CamelContext context=new DefaultCamelContext();
+		
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				
+				from("direct:a")
+			    .marshal(new ReverseDataFormat())
+			    .log("${body}");
+				
+			}
+			
+		});
+		
+		context.start();
+		
+		ProducerTemplate template=context.createProducerTemplate();
+		
+		template.sendBody("direct:a", "marius");
+		
+		context.stop();
+		
+		
+	}
+	
+
+}
